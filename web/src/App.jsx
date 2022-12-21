035230f9be925520b9fa209b1b2291d02258aeb0
Otávio Augusto <otavioaugustobrb19@gmail.com>
2022-12-21 15:46:27 -0300

    first commit?

---
+++ web/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/img/react.svg'
+import { Menu } from './components/Menu'
+//import './App.css'
+import './assets/css/main.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Menu/>
+    </div>
+  )
+}
+
+export default App
---
+++ web/src/components/Menu.jsx
@@ -0,0 +1,13 @@
+export function Menu() {
+    return (
+        <div className="menu">
+
+            <h2 className="menu__heading">Search Here For Your Item</h2>
+
+            <input className="menu__input input" placeholder="search here..."></input>
+
+            <button className="btn menu__btn">Search</button>
+
+        </div>
+    )
+}
